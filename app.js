1a788e5229079812e5119fc0733acf844e1aba97
Cagri Acar <cagriacar22@gmail.com>
2021-09-19 03:40:34 +0300

    Proje eksikleri var

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/* https://api.genderize.io?name=peter */
 const nameInput = document.querySelector(".input-name");
 const btnGuess = document.querySelector(".btn-guess");
 const prediction = document.querySelector(".prediction");
@@ -9,17 +8,20 @@ const getAPI = (name) => {
       return response.json();
     })
     .then((data) => {
-        console.log(data.country[0].probability)
-            prediction.style.display = "block";
-            nameInput.value = "";
-            prediction.innerHTML = `
-                <p>  you are probably % ${(data.country[0].probability *100).toFixed(0)} <span> ${data.country[0].country_id}</span> !!!' </p>
+      console.log(data.country[0].probability);
+      prediction.style.display = "block";
+      nameInput.value = "";
+      prediction.innerHTML = `
+                <p>  you are probably % ${(
+                  data.country[0].probability * 100
+                ).toFixed(0)} <span> ${
+        data.country[0].country_id
+      }</span> !!!' </p>
          `;
-    });     
+    });
 };
 const getGender = () => {
   getAPI(nameInput.value);
 };
 
 btnGuess.addEventListener("click", getGender);
-/*   <p>Olasılıkla yüzde % 90 <span>Erkek</span>'siniz !!!' </p> */
